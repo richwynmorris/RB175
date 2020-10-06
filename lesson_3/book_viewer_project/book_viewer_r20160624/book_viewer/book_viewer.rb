4d75a9dd6eafda838ef192f1e063188ade3efb0f
Richard Morris <rwmorris.bjj@gmail.com>
2020-10-06 22:12:20 +0100

    working through sinatra project

--- lesson_3/book_viewer_project/book_viewer_r20160624/book_viewer/book_viewer.rb
+++ lesson_3/book_viewer_project/book_viewer_r20160624/book_viewer/book_viewer.rb
@@ -2,16 +2,71 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+def each_chapter
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+def chapters_matching(query)
+  results = []
+
+  return results if !query || query.empty?
+
+  each_chapter do |number, name, contents|
+    results << {name: name, number: number} if contents.include?(query)
+  end
+
+  results
+end
+
+not_found do
+  redirect '/'
+end
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlock"
   @contents = File.readlines("data/toc.txt")
   erb :home
 end
 
-get "/chapters/1" do
-  @chapter = "Chapter 1"
-  @contents = File.readlines("data/toc.txt")
-  @content = File.readlines("data/chp1.txt")
+get "/chapters/:number" do
+  number = params[:number]
+  @title = "Chapter #{number}"
+
+
+  redirect "/" unless (1..@contents.size).cover? number
+  
+  @content = File.read("data/chp#{number}.txt")
+  @chapter_name = @contents[number.to_i - 1]
 
   erb :chapter
 end
+
+get "/show/:name" do
+  params[:name]
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+
+  erb :search
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end.join
+  end
+end
+
+
+
+
